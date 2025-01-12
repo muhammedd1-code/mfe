c8b7e1882a902d7fcf41bccfc11b1d5f4dda75a1
Muhammed Arafath <muhammedarafath@Muhammeds-MacBook-Air-2.local>
2025-01-13 02:38:55 +0530

    updated config deployment script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,5 +1,6 @@
 const {merge} = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
@@ -18,6 +19,9 @@ const prodConfig = {
             },
             shared: packageJson.dependencies,
         }),
+        new HtmlWebpackPlugin({
+                template: './public/index.html'
+            })
     ],
 };
 
