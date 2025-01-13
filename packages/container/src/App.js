d1a12ccceba0b2d596e72ae3619c0db68be5b4b8
Muhammed Arafath <muhammedarafath@Muhammeds-MacBook-Air-2.local>
2025-01-13 20:43:10 +0530

    updated comments

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return <div>
-    <h1>Hi there!!</h1>
+    <h1>Hi there!!, I Love you Pravii!!!</h1>
     <hr />
     <MarketingApp />
     </div>
