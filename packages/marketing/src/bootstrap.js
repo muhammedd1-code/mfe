3e9431cb06efe18dd18008b099fec52a16e36764
Muhammed Arafath <muhammedarafath@Muhammeds-MacBook-Air-2.local>
2025-01-14 17:30:03 +0530

    Updated routes, communication

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './app';
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App/>,
+        <App history={history}/>,
         el
     );
-}
+
+    return {
+        onParentNavigate({pathname: nextPathname}){
+            const { pathname } = history.location;
+            if(pathname !== nextPathname){
+                history.push(nextPathname);
+            }
+        }
+    }
+}    
+
 // If we are in development and in isolation, call mount immediately
 if(process.env.NODE_ENV === 'development'){
     const el = document.querySelector('#_marketing-dev-root');
     if(el){
-        mount(el);
+        mount(el, { defaultHistory: createBrowserHistory() });
     }
 }
 
